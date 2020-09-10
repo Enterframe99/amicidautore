1da419b35c216091299903277d6b9735f789d491
claud <wm@kmz.it>
2020-09-10 15:38:18 +0200

    10 setttembre

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
          //$this->call(UserSeeder::class);
-        \App\Models\Book::truncate();
          $this->call(BookSeeder::class);
     }
 }
